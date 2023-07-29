eac4d7b88ca6735f50eb92030a3d0403bce06add
AnnelisseM <AnnelisseM@CIA>
2023-07-29 06:49:26 -0700

    Initiate Commit

---
+++ src/prplaces/WhereToGo.java
@@ -0,0 +1,8 @@
+package prplaces;
+
+public class WhereToGo {
+	public static void main(String[] args) {
+		System.out.println("El Viejo San Juan, El Yunque, Flamenco Beach, Puerto Nuevo Beach, Combate Beach");
+	}
+
+}
